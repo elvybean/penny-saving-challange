6a84475baa9f512e1848a7aaecfb5388f4ec6886
Elvis Obero-Atkins <96076897+elvybean@users.noreply.github.com>
2022-08-13 03:04:56 +0100

    revisions (1)

--- main/finalrev.py
+++ main/finalrev.py
@@ -1,5 +1,12 @@
-from module import daily, weekly
+def daily(i: int, j: int):
+    print("Today I have saved: £",round(float(i/100), 2),
+          " in total that makes: £",round(j, 2))
 
+def weekly(i: int, j: int, k: int):
+    if i % 7 == 0:
+        print("In week ",int(i/7),"This week you have saved: £", round(j - k, 2))
+        k = j
+        
 
 if __name__ == "__main__":
     j = 0
